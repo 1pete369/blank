f2bec1a34f24cda5081e7a98703bd2cc6a2d35d7
REDDY PRO <youremail@example.com>
2025-06-05 18:22:01 +0530

    Setup Expo + TypeScript + Tailwind + core libraries

--- App.tsx
+++ App.tsx
@@ -1,20 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from "react-native"
+import React from "react"
+import "./global.css"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text className=" text-3xl bg-sky-500">App</Text>
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
